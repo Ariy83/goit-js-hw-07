08d59cdfa40df9047fc469cdd3ac9e5e5a5816a1
Ariy83 <143614766+Ariy83@users.noreply.github.com>
2023-11-02 17:24:12 +0200

    +02

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,28 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryListEl = document.querySelector("ul.gallery");
+
+function makeGallery(galleryItems) {
+  return galleryItems.map(({ preview, original, description }) => {
+    return `<li class="gallery__item">
+   <a class="gallery__link" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+   </a>
+</li>`;
+  });
+}
+
+function renderGallery(galleryItems) {
+  const markup = makeGallery(galleryItems).join("");
+  galleryListEl.insertAdjacentHTML("afterbegin", markup);
+}
+renderGallery(galleryItems);
+
+let lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionPosition: "bottom",
+  captionDelay: 250,
+});
